b736e169c3ecf5dedf1ce9aa91824821a6848ffc
Pau Tudela Lorenzo <pau.tudela@tresipunt.com>
2023-04-25 12:37:26 +0200

    Step 3 logica de guardado de datos en progreso 2

---
+++ amd/src/origin_restore_course_step3.js
@@ -0,0 +1,79 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package
+ * @author  2022 3iPunt <https://www.tresipunt.com/>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
+define([
+    'jquery',
+    'core/str',
+    'core/ajax',
+    'core/templates'
+], function($, Str, Ajax, Templates) {
+    "use strict";
+
+    let ACTIONS = {
+        COURSE_SELECT: '[data-action="select"]',
+        COURSE: '[data-action="course"]',
+        NEXT: '[data-action="next"]'
+    };
+
+    /**
+     * @param {String} region
+     *
+     * @constructor
+     */
+    function restoreCourseStep3(region) {
+        this.node = $(region);
+        let data = {
+            course: {
+                id: parseInt($("[data-course-id]").attr("data-course-id")),
+                fullname: $("[data-course-fullname]").attr("data-course-fullname"),
+                shortname: $("[data-course-shortname]").attr("data-course-shortname"),
+                idnumber: $("[data-course-idnumber]").attr("data-course-idnumber"),
+                categoryid:  parseInt($("[data-course-categoryid]").attr("data-course-categoryid")),
+                categoryname: $("[data-course-categoryname]").attr("data-course-categoryname"),
+                backupsizeestimated:  parseInt($("[data-course-backupsizeestimated]").attr("data-course-backupsizeestimated")),
+                sections: []
+            }
+        };
+        this.node.find(ACTIONS.NEXT).on('click', this.clickNext.bind(this));
+        // console.log(data);
+        console.log($("[data-section-sectionname]").attr("data-section-sectionname"));
+    }
+
+    restoreCourseStep3.prototype.clickNext = function(e) {
+        // TODO: logica del guardado de datos en un json y guardarlos en session storage.
+    };
+
+    restoreCourseStep3.prototype.node = null;
+
+    return {
+        /**
+         * @param {String} region
+         * @return {restoreCourseStep3}
+         */
+        initRestoreCourseStep3: function(region) {
+            // eslint-disable-next-line babel/new-cap
+            return new restoreCourseStep3(region);
+        }
+    };
+});
