18ea03150496b3c0a66a63e94e82f536606a5a72
Antonio Manzano <amanzano@tresipunt.com>
2024-06-04 09:01:02 +0200

    up version 2024060400

--- version.php
+++ version.php
@@ -33,7 +33,7 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024051700;
+$plugin->version   = 2024060400;
 $plugin->requires  = 2021051703;
 $plugin->component = 'local_coursetransfer';
 $plugin->release   = '1.1.0';
